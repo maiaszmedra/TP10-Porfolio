35cc19d6e38355a8cd3ba5179fe488bc0689f24f
MicaelaCembal <47297659@est.ort.edu.ar>
2023-11-22 14:19:06 -0300

    terminadoooou

--- src/Info.js
+++ src/Info.js
@@ -5,12 +5,12 @@ import "./Info.css"
 export default function Info() {
     return (
         <div class="icontainer">
-            <div class="container">
-                <div class="palette">
-                    <div class="color"><span>Sobre mi</span></div>
-                    <div class="color"><span>2A9D8F</span></div>
-                    <div class="color"><span>E9C46A</span></div>
-                </div>
+            <div class="palette">
+                <div class="color"><span class="texto">Soy Maia Szmedra, una alumna en su último año de ORT, sede Almagro</span></div>
+                <div class="color"><span class="texto">Me considero una persona creativa, resolutiva y con capacidad de adaptarme a distintas circumstancias</span></div>
+                <div class="color"><span class="texto">Hablo Inglés con nivel avanzado y hebreo con nivel inicial</span></div>
+                <div class="color"><span class="texto">Tengo experiencia trabajando como ayudante en un estudio contable</span></div>
+                <div class="color"><span class="texto">Gracias por leer!<img src="../yo.jpg" class="img"/> </span></div>
             </div>
         </div>
     )
